1519f5fe38dd59cb21d56e4df4556c7a31827119
Vincent Onderwaater <vincent@onderwaater.nl>
2018-04-02 21:11:12 +0200

    var

--- app/assets/javascripts/menu/menu.js
+++ app/assets/javascripts/menu/menu.js
@@ -6,34 +6,34 @@ function handleHrefClick(e, href) {
 function showPopup(href) {
   if (window.showing) window.showing.classList.remove('show');
   if (window.selectedLink) window.selectedLink.classList.remove('active');
-  let mainmenu = document.querySelector('.navigation-menu');
+  var mainmenu = document.querySelector('.navigation-menu');
   mainmenu.classList.add('hide');
-  let a = document.querySelector('li.' + href + ' a');
+  var a = document.querySelector('li.' + href + ' a');
   a.classList.add('active');
   window.selectedLink = a;
-  let maincontent = document.querySelector('.main-content');
+  var maincontent = document.querySelector('.main-content');
   maincontent.classList.add('show');
-  let section = document.querySelector('.content-section.' + href);
+  var section = document.querySelector('.content-section.' + href);
   section.classList.add('show');
   window.showing = section;
 }
 
 function resetPopup() {
-  let mainmenu = document.querySelector('.navigation-menu');
+  var mainmenu = document.querySelector('.navigation-menu');
   mainmenu.classList.remove('hide');
-  let maincontent = document.querySelector('.main-content');
+  var maincontent = document.querySelector('.main-content');
   maincontent.classList.remove('show');
   window.showing.classList.remove('show');
 }
 
-let startIndex = 1;
-let endIndex = 10;
+var startIndex = 1;
+var endIndex = 10;
 
 setInterval(function () {
-  let idx = Math.ceil(Math.random() * (endIndex - startIndex));
-  let body = document.querySelector('body');
-  let img = new Image();
-  let imgUrl = "/bg/bg"  + idx + ".jpg";
+  var idx = Math.ceil(Math.random() * (endIndex - startIndex));
+  var body = document.querySelector('body');
+  var img = new Image();
+  var imgUrl = "/bg/bg"  + idx + ".jpg";
   img.addEventListener('load', function() {
     body.style.backgroundImage = "url('" + imgUrl + "')";
   });
